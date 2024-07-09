00a6863587f511698c1fcfea9dc57548a8196cbd
ibrahim <ibrahimrabbihere@gmail.com>
2024-07-09 17:16:00 +0600

    this is the end application

--- src/app/error/globalErrorHandler.ts
+++ src/app/error/globalErrorHandler.ts
@@ -5,27 +5,22 @@ import { NextFunction, Request, Response } from "express";
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     let statusCode = 404
     let message = err.message;
-  
+    let errSource = err.details || err
+     
  
     if (err.details) {
         statusCode = 400
     }
 
-    if (Object.keys(err).length) {
+    if (err.statusCode) {
         statusCode = err.statusCode
-        message = err.message
     }
-
-     
-
-    
-
     
     return res.status(statusCode).json({
         success: false,
         statusCode : statusCode,
         message: message,
-        errSource: err
+        errSource: errSource
     })
 };
 
